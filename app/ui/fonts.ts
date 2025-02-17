012f17dde43a2e2206cce3d34236296090e8f756
Danial Sadiq Masood <danialsadiqmasood44@gmail.com>
2025-02-18 04:02:32 +0500

    add lusitana

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -3,7 +3,8 @@ import {Inter, Rubik, Lusitana} from 'next/font/google'
 export const inter = Inter({subsets : ['latin']})
 
 export const lusitana = Lusitana({
-    weight : ['400', '700']
+    weight : ['400', '700'],
+    subsets : ['latin']
 })
 
 export const rubik = Rubik({
